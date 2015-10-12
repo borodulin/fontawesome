9e35e534687fd630146aa14e7a99c0f0844bef27
Andrey Borodulin <borodulin@gmail.com>
2015-10-12 21:50:22 +0600

    path update

--- FontAwesomeAsset.php
+++ FontAwesomeAsset.php
@@ -13,8 +13,8 @@ class FontAwesomeAsset extends \yii\web\AssetBundle
 {
 	// The files are not web directory accessible, therefore we need
 	// to specify the sourcePath property. Notice the @bower alias used.
-	public $sourcePath = '@bower/font-awesome';
+	public $sourcePath = '@bower/fontawesome';
 	public $css = [
-			'css/font-awesome.css',
+			'css/font-awesome.min.css',
 	];
 }
